88a439dd962ebb7ec6462092ae652c257edc3709
Dayane Ferreira <109527034+ladyenay@users.noreply.github.com>
2022-08-01 13:54:30 -0300

    enviando mensagem e apresentando mensagem de sucesso

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 });
+
+function enviarMensagem() {
+    let nome = document.querySelector('[name="nome"]').value;
+    let email = document.querySelector('[name="email"]').value;
+    let mensagem = document.querySelector('[name="mensagem"]').value;
+
+    console.log('Enviado mensagem...: ', { nome, email, mensagem });
+    
+    limparFormulario();
+
+    alert("Até mais e obrigado pelos peixes!");
+}
+
+function limparFormulario() {
+    document.querySelector('[name="nome"]').value = '';
+    document.querySelector('[name="email"]').value = '';
+    document.querySelector('[name="mensagem"]').value = '';
+}
